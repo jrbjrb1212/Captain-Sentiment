f81804284b9f920075a51f702fa55e29d8b43ea8
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-05-30 01:22:18 -0400

    Rescrap

--- Corpus/Scrap.py
+++ Corpus/Scrap.py
@@ -67,7 +67,7 @@ def format_output(record, name):
                 # if word.isalpha():
                 out.write(word.lower() + "\t")
                 i += 1
-                if i == wordcount // 10:
+                if i == wordcount // 40:
                     out.write("\n")
                     i = 0
         out.write("\nWord count: " + str(wordcount) + " words")
