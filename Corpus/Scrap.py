fdf93d435276aa5efecd5588e214e890c0888910
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-05-29 20:40:29 -0400

    Load the webscrap

--- Corpus/Scrap.py
+++ Corpus/Scrap.py
@@ -1,12 +1,15 @@
 import requests
 import bs4
 
+# r = requests.get("https://transcripts.foreverdreaming.org/viewtopic.php?f=953&t=36758&sid=e688625cc6f46858a91098d39640303c")
+# print(r.text)
 
-
-with open("websites.txt", "r'") as f:
+# open txt file containing all URLs for movies to strip
+with open("websites.txt", "r") as f:
     for line in f:
-        if line.find("https"):
-            r = requests.get(f.readline)
+        if line.find("https") != -1:
+            r = requests.get(line)
+            print("Done stripping")
         else:
             print("Now stripping " + line)
 
