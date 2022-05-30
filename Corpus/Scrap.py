ac17c8c02213d8bf07380dc5840d4383015c89a7
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-05-29 21:40:28 -0400

    output file

--- Corpus/Scrap.py
+++ Corpus/Scrap.py
@@ -1,3 +1,4 @@
+from black import main
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -5,20 +6,39 @@ import re
 # r = requests.get("https://transcripts.foreverdreaming.org/viewtopic.php?f=953&t=36758&sid=e688625cc6f46858a91098d39640303c")
 # print(r.text)
 
-# open txt file containing all URLs for movies to strip
 
-with open("websites.txt", "r") as f:
-    with open("test.txt", "w") as out:
+# open txt file containing all URLs for movies to strip
+def main(self=None):
+    with open("websites.txt", "r") as f:
         for line in f:
+            record = []
             if line.find("https") != -1:
                 r = requests.get(line)
                 doc = BeautifulSoup(r.text, "html.parser")
 
                 for tag in doc.findAll('p'):
-                    if tag.name != 'p':
+                    if tag.name not in 'p':
                         tag = tag.replaceWith(tag.renderContents())
-                    out.write(tag.renderContents().decode('UTF-8') + "\n")
-
+                    record.append(tag.renderContents().decode('UTF-8'))
+                format_output(record)
                 print("Done stripping")
+
             else:
                 print("Now stripping " + line)
+
+
+def format_output(record):
+    # remove first entry and last 3 entries as they are not actual script
+    record = record[1:-4]
+    i = 0
+    with open("test.txt", "w") as out:
+        for line in record:
+            for word in line.split():
+                out.write(word.lower() + "\t")
+                i += 1
+                if i == 20:
+                    out.write("\n")
+                    i = 0
+
+
+main()
