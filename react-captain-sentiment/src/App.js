899470c5425fd90d6d316c598985e17a361a7127
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-06-11 23:07:50 -0400

    Inital Header

--- react-captain-sentiment/src/App.js
+++ react-captain-sentiment/src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header"
+// start with header work down from there
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
+      <Header />
     </div>
   );
 }
---
+++ react-captain-sentiment/src/components/Header.js
@@ -0,0 +1,24 @@
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>Captain Sentiment</h1>
+            <ul>
+                <li><a href=""> About </a></li>
+                <li><a href=""> Data Interpretation </a></li>
+                <li><a href=""> Technologies Used </a></li>
+                <li><a href=""> Github </a></li>
+
+            </ul>
+            {/* <h4><a href=""> About </a></h4>
+            <h4><a href=""> Data Interpretation </a></h4>
+            <h4><a href=""> Technologies Used </a></h4>
+            <h4><a href=""> Github </a></h4> */}
+            
+        </header>
+    )
+}
+
+export default Header
+
