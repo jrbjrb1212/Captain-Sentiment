1a26cca96ce71ef3a72119eaf53b386747691658
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-06-25 13:31:05 -0400

    Chart updates and fleshing out

--- react-captain-sentiment/src/components/Cardwhole/Chart.js
+++ react-captain-sentiment/src/components/Cardwhole/Chart.js
@@ -1,9 +1,34 @@
-const Chart = () => {
-    return (
-        <>
-           <p>Chart Goes Here</p> 
-        </>
-    )
+import React, {Component} from 'react';
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+
+class Chart extends Component {
+
+    static defaultProps = {
+        displayTitle:true,
+        displayLegend: true,
+    }
+    
+
+    render() {
+        return(
+            <div className="chart">
+                Hello
+            </div>
+        )
+    }
 }
 
-export default Chart
+export default Chart;
+
--- react-captain-sentiment/src/components/Header.js
+++ react-captain-sentiment/src/components/Header.js
@@ -2,13 +2,6 @@ import React from 'react'
 import Header_nav from './Header_Nav'
 
 const Header = () => {
-    // <h1>Captain Sentiment</h1>
-    //         <ul>
-    //             <li><a href="" > About </a></li> {/* Short page about how this project came to be */}
-    //             <li><a href=""> Data Interpretation </a></li> {/* Create an data interpretation page later */}
-    //             <li><a href=""> Technologies Used </a></li> {/* Create page with the technologies used later Somewhat like a readme */}
-    //             <li><a href=""> Github </a></li> {/* Fill with public Github Link later */}     
-    //         </ul>
     return (
         <section className="header">
             <section className="header-top">
